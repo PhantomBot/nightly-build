0fb7da5742932e5efdb845f5188e1c3f43843ac4
gmt2001 <gmt2001@users.noreply.github.com>
2020-01-10 08:47:28 -0500

    Fixed nightlyPBBuild.sh

--- scripts/nightlyPBBuild.sh
+++ scripts/nightlyPBBuild.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 #
 # Copyright (C) 2016-2020 phantombot.tv
 #
@@ -39,28 +39,32 @@ LAST_REPO_VERSION=$(cat last_repo_version)
 
 cd ${HOME}
 
-git clone https://github.com/PhantomBot/PhantomBot.git 2>/dev/null 1>&2
+git clone --progress --depth=1 https://github.com/PhantomBot/PhantomBot.git
 cd PhantomBot
 PB_VERSION=$(grep "property name=\"version\"" build.xml | perl -e 'while(<STDIN>) { ($ver) = $_ =~ m/\s+<property name=\"version\" value=\"(.*)\" \/>/; } print $ver;')
-ant distclean clean 2>/dev/null 1>&2
-ant -Dnightly=nightly_build -Dversion=${PB_VERSION}-NB-$(date +%Y%m%d) 2>/dev/null 1>&2
+ant -noinput -buildfile build.xml distclean clean
+ant -noinput -buildfile build.xml -Dbuildtype=nightly_build -Dversion=${PB_VERSION}-NB-$(date +%Y%m%d) dist
 if [[ $? -ne 0 ]]; then
     exit 1
 fi
 REPO_VERSION=$(git rev-parse --short HEAD)
-cp -f ${MASTER}/PhantomBot/dist/PhantomBot*zip ${BUILDS}/${BUILD}
-cp -f ${MASTER}/PhantomBot/dist/PhantomBot*zip ${HISTORICAL}/${BUILD_DATED}
+cp -f ${MASTER}/dist/PhantomBot*zip ${BUILDS}/${BUILD}
+cp -f ${MASTER}/dist/PhantomBot*zip ${HISTORICAL}/${BUILD_DATED}
 
 PBFOLDER=PhantomBot-${PB_VERSION}-NB-$(date +%Y%m%d)
 
-cd ${MASTER}/PhantomBot/dist/
-zip -r ${BUILDS}/${LIN_BUILD} ${PBFOLDER} -x 'java-runtime/*' -x 'java-runtime-macos/*' -x 'launch.bat'
-zip -r ${BUILDS}/${WIN_BUILD} ${PBFOLDER} -x 'java-runtime-linux/*' -x 'java-runtime-macos/*' -x 'launch.sh' -x 'launch-service.sh'
-zip -r ${BUILDS}/${MAC_BUILD} ${PBFOLDER} -x 'java-runtime-linux/*' -x 'java-runtime/*' -x 'launch.bat'
-zip -r ${BUILDS}/${ARM_BUILD} ${PBFOLDER} -x 'java-runtime-linux/*' -x 'java-runtime/*' -x 'java-runtime-macos/*' -x 'launch.bat'
+cd ${MASTER}/dist/
+
+echo "Lin zip"
+zip -r ${BUILDS}/${LIN_BUILD} ${PBFOLDER} -x '*java-runtime/*' -x '*java-runtime-macos/*' -x '*launch.bat'
+echo "Win zip"
+zip -r ${BUILDS}/${WIN_BUILD} ${PBFOLDER} -x '*java-runtime-linux/*' -x '*java-runtime-macos/*' -x '*launch.sh' -x '*launch-service.sh'
+echo "Mac zip"
+zip -r ${BUILDS}/${MAC_BUILD} ${PBFOLDER} -x '*java-runtime-linux/*' -x '*java-runtime/*' -x '*launch.bat'
+echo "Arm zip"
+zip -r ${BUILDS}/${ARM_BUILD} ${PBFOLDER} -x '*java-runtime-linux/*' -x '*java-runtime/*' -x '*java-runtime-macos/*' -x '*launch.bat'
 
 cd ${BUILDS}
-git pull 2>/dev/null 1>&2
 if [[ "${LAST_REPO_VERSION}" = "${REPO_VERSION}" ]]; then
     export BUILD_STR="${COMMITSTR} (Repo: ${REPO_VERSION}) (No Changes)"
 else
@@ -70,14 +74,17 @@ cat builds.md | perl -e 'while(<STDIN>) { if ($_ =~ /------/ ) { print $_; print
 head -25 builds.new > builds.md
 rm -f builds.new
 echo ${REPO_VERSION} > last_repo_version
-git add ${BUILD} ${LIN_BUILD} ${WIN_BUILD} ${MAC_BUILD} ${ARM_BUILD} historical/${BUILD_DATED} builds.md last_repo_version 2>/dev/null 1>&2
+git config user.email "PhantomBot-Nightly-build@github-actions.local"
+git config user.name "GitHub-Actions/PhantomBot/nightly-build"
+git add ${BUILD} ${LIN_BUILD} ${WIN_BUILD} ${MAC_BUILD} ${ARM_BUILD} historical/${BUILD_DATED} builds.md last_repo_version
 cd ${BUILDS}/historical
-find . -mtime +20 -exec git rm {} \; 2>/dev/null 1>&2
-git commit -m "${BUILD_STR}" 2>/dev/null 1>&2
+find . -mtime +20 -exec git rm {} \;
+git commit -m "${BUILD_STR}"
 if [[ "${DRY_RUN}" = "false" ]]; then
-    git push "https://${GITHUB_ACTOR}:${TOKEN_GITHUB}@github.com/${GITHUB_REPOSITORY}.git" 2>/dev/null 1>&2
+    git push "https://${GITHUB_ACTOR}:${TOKEN_GITHUB}@github.com/${GITHUB_REPOSITORY}.git"
 else
+    cd ${BUILDS}
     echo "$(ls)"
-    echo.
+    echo ""
     echo "$(unzip -l ${ARM_BUILD})"
 fi
