f6ddce7872896d0cf1da4e7b0b1830ad627df2b7
gmt2001 <gmt2001@users.noreply.github.com>
2020-02-15 02:22:05 -0500

    Fix sed commands

--- scripts/nightlyPBDocker.sh
+++ scripts/nightlyPBDocker.sh
@@ -47,9 +47,9 @@ cd ${DOCKER_BUILD}
 rm -rf resources/java-runtime
 rm -rf resources/java-runtime-macos
 
-sed -i "s/&& apt-get update -q \\//" Dockerfile
-sed -i "s/&& apt-get install -yqq ant \\/" Dockerfile
-sed -i "s/&& apt-get clean \\//" Dockerfile
+sed -i "/&& apt-get update -q \\\\/d" Dockerfile
+sed -i "/&& apt-get install -yqq ant \\\\/d" Dockerfile
+sed -i "/&& apt-get clean \\\\/d" Dockerfile
 sed -i "s/ant jar/ant -noinput -buildfile build.xml -Dbuildtype=nightly_build -Dversion=${PB_VERSION}-NB-${DATE} jar/" Dockerfile
 sed -i "s/\/dist\/build\//\/dist\/${PBFOLDER}\//" Dockerfile
 
